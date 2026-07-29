8b5a677886ddfe93ae671fbe13446837abaced95
Bharath R Shet <30365864+bc1213@users.noreply.github.com>
2026-07-29 10:29:35 +0530

    Update LiveBroadcast.jsx

--- chaturmasya_demo/src/pages/LiveBroadcast.jsx
+++ chaturmasya_demo/src/pages/LiveBroadcast.jsx
@@ -10,7 +10,7 @@ export default function LiveBroadcast() {
     const initializePlayer = () => {
       if (!window.YT || !window.YT.Player || !playerRef.current || youtubePlayerRef.current) return;
       youtubePlayerRef.current = new window.YT.Player(playerRef.current, {
-        videoId: "Y3jT7HIw3lI",
+        videoId: "HaDsGZTVZEw",
         playerVars: { 
           autoplay: 1, 
           mute: 1, 
